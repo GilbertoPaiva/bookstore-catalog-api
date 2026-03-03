88a915a5f0d7661ae222ad1370dee8bcaa6737cd
GilbertoPaiva <gilberto.melo.paiva@gmail.com>
2026-03-02 21:34:39 -0300

    Initial commit

---
+++ src/main/java/com/gilbertopaiva/bookstore_catalog_api/BookstoreCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.gilbertopaiva.bookstore_catalog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreCatalogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gilbertopaiva/bookstore_catalog_api/BookstoreCatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilbertopaiva.bookstore_catalog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreCatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
